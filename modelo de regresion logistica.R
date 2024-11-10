1aedb073918c64be2b9ace9f89b2208c8da2239e
Andy-Henao <59206452+Andy-Henao@users.noreply.github.com>
2024-11-10 09:14:50 -0500

    Update 1 modelo de regresion logistica.R

--- modelo de regresion logistica.R
+++ modelo de regresion logistica.R
@@ -8,7 +8,8 @@ library(pROC)
 library(Metrics)
 
 #cambiar el camino al archivo dependiendo de donde lo guardaras
-water_data <- read.csv("Q_Q_plots_for_normality/water_potability.csv")
+water_data <- read.csv("D:/IngenieriaMatematica/Semestre5/Estadistica2/Proyecto/water_potability.csv")
+
 View(water_data)
 
 # Transformación de la variable "Solids" (raíz cuadrada) para cumplir la normalidad
@@ -35,23 +36,41 @@ model <- glm(Potability ~ ph + Hardness + Solids + Chloramines + Sulfate + Condu
 # Resumen del modelo
 summary(model)
 
+# Extraer los coeficientes del modelo (opcional)
+coeficientes <- coef(model)
+print(coeficientes)
+
+# Calcular las probabilidades utilizando predict() en lugar de una función personalizada
+train_data$Probability_of_Potability <- predict(model, type = "response")
+
+# Verificar el dataframe con la nueva columna de probabilidades
+View(train_data)
+
 # Predicciones en los conjuntos de prueba y validación
-test_data$predicted_prob <- predict(model, newdata = test_data, type = "response")
-validation_data$predicted_prob <- predict(model, newdata = validation_data, type = "response")
+test_data$Probability_of_Potability <- predict(model, newdata = test_data, type = "response")
+validation_data$Probability_of_Potability <- predict(model, newdata = validation_data, type = "response")
+
+
+# Unir los dataframes en uno solo
+water_potability <- rbind(train_data, test_data, validation_data)
+
 
 # Categorización en base a percentiles (opcional)
-percentile_33 <- quantile(test_data$predicted_prob, 0.33)
-percentile_66 <- quantile(test_data$predicted_prob, 0.66)
+percentile_33 <- quantile(water_potability$Probability_of_Potability, 0.33)
+percentile_66 <- quantile(water_potability$Probability_of_Potability, 0.66)
 
-test_data <- test_data %>%
+water_potability <- water_potability %>%
   mutate(Risk_Category = case_when(
-    predicted_prob <= percentile_33 ~ "Alto riesgo de no potabilidad",
-    predicted_prob > percentile_33 & predicted_prob <= percentile_66 ~ "Moderado",
-    predicted_prob > percentile_66 ~ "Bajo riesgo de no potabilidad"
+    Probability_of_Potability <= percentile_33 ~ "Alto riesgo de no potabilidad",
+    Probability_of_Potability > percentile_33 & Probability_of_Potability <= percentile_66 ~ "Moderado",
+    Probability_of_Potability > percentile_66 ~ "Bajo riesgo de no potabilidad"
   ))
 
+View(water_potability)
+
+
 # Evaluación del desempeño del modelo: matriz de confusión y precisión
-predicted_classes <- ifelse(test_data$predicted_prob > 0.5, 1, 0)
+predicted_classes <- ifelse(test_data$Probability_of_Potability > 0.5, 1, 0)
 conf_matrix <- table(test_data$Potability, predicted_classes)
 sensitivity <- conf_matrix[2, 2] / sum(conf_matrix[2, ])
 specificity <- conf_matrix[1, 1] / sum(conf_matrix[1, ])
@@ -63,13 +82,13 @@ print(paste("Precisión (Valor predictivo positivo) en el conjunto de prueba:", 
 print(paste("Precisión del modelo en el conjunto de prueba:", accuracy))
 
 # Visualización de la distribución de las probabilidades
-ggplot(test_data, aes(x = predicted_prob, fill = Risk_Category)) +
+ggplot(water_potability, aes(x = Probability_of_Potability, fill = Risk_Category)) +
   geom_histogram(binwidth = 0.05) +
   labs(title = "Distribución de la probabilidad de potabilidad en el conjunto de prueba", x = "Probabilidad de Potabilidad", y = "Frecuencia") +
   theme_minimal()
 
 # curva ROC y AUC para evaluar la capacidad del modelo de distinguir entre clases
-roc_curve <- roc(test_data$Potability, test_data$predicted_prob)
+roc_curve <- roc(test_data$Potability, test_data$Probability_of_Potability)
 plot(roc_curve, main = "Curva ROC para el conjunto de prueba")
 auc_value <- auc(roc_curve)
 print(paste("AUC del modelo en el conjunto de prueba:", auc_value))
